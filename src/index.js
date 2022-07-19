20a927844b55f2843ab32e06a96b865cd835c5c9
davis_gaunt <bdg0015@uah.edu>
2022-07-19 09:31:47 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route,  } from "react-router-dom"
+import { Provider } from 'react-redux'
+import App from './App'
+import store from './app/store'
+
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+)

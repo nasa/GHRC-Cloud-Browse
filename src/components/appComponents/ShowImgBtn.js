e81f18893f818c17894a75bcab8677fe78f3a81f
davis_gaunt <bdg0015@uah.edu>
2022-07-28 16:03:23 -0500

    added show image functionality

--- src/components/appComponents/ShowImgBtn.js
+++ src/components/appComponents/ShowImgBtn.js
@@ -1,21 +1,43 @@
 import { Box, Button } from '@mui/material'
 import Backdrop from '@mui/material/Backdrop'
 import React from 'react'
+import config from '../../config'
+import { useSelector } from 'react-redux'
 
 const ShowImgBtn = () => {
-        //not finnished until bucket structure is complete
-
+       //not finnished until bucket structure is complete
+    const selectedList = useSelector(state => state.selectedList.value)
+    const delim = useSelector(state => state.delim.value)
     const [open, setOpen] = React.useState(false);
+
+    const checkDisabled = () => {
+        if(selectedList.length !== 1){
+            return true
+        }else if (delim === '/'){
+            return true
+        }else if (typeof selectedList[0]["Key"] === 'undefined'){
+            return true
+        }else if (selectedList[0]["Key"].split('.').pop() === 'jpg'){
+            return false
+        } else {
+            console.log('true')
+            return true
+        }
+    }
+
     const handleClose = () => {
         setOpen(false);
-    };
+    }
+
     const handleToggle = () => {
         setOpen(!open);
-    };
+    }
+
     return (
         <>
             <Button variant='outlined' sx={{ml: 15}}
-            onClick={handleToggle}>
+            onClick={handleToggle}
+            disabled= {checkDisabled()}>
                 Show Image
             </Button>
             <Backdrop
@@ -24,7 +46,7 @@ const ShowImgBtn = () => {
                 onClick={handleClose}
             >
                 <Box component='img'
-                    src='https://picsum.photos/800/600' 
+                    src={checkDisabled() ? '' :`${config.cloudWatchUrlBase}${selectedList[0]['Key']}`} 
                     />
             </Backdrop>
         </>

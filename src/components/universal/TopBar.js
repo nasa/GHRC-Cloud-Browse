8566bffb939f7fb442bf5f0b54ef3b3b9a5db9bd
davis_gaunt <bdg0015@uah.edu>
2022-07-29 11:44:16 -0500

    demo 7/29 with bread crumbs

--- src/components/universal/TopBar.js
+++ src/components/universal/TopBar.js
@@ -4,7 +4,7 @@ import React from 'react'
 const TopBar = () => {
 
   return (
-    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+    <AppBar position="relative"sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, bgcolor: '#2276AC' }}>
         <Toolbar>
             <Box
             sx={{width: 85}}

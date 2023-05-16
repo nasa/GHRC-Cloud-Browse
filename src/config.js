4b1c03d400071ce8080c7149cf84a748d2b930c7
Navaneeth R Selvaraj <navaneeth026@gmail.com>
2023-05-16 15:22:36 -0500

    adding source url for get link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    cloudWatchUrlBase: 'https://d29dkgeibpazuv.cloudfront.net/'
+    cloudWatchUrlBase: 'https://d29dkgeibpazuv.cloudfront.net/',
+    sourceIMGUrl: 'https://data.ghrc.earthdata.nasa.gov/ghrcw-public/'
 }
 
 export default config

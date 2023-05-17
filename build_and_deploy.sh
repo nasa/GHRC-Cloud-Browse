d948a6b0925da5a887d98618c022e68258dfc77a
Navaneeth R Selvaraj <navaneeth026@gmail.com>
2023-05-17 13:26:34 -0500

    adding aws variables to script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+export AWS_REGION=$bamboo_AWS_REGION
+export AWS_ACCESS_KEY_ID=$bamboo_AWS_SVC_PROD_SECRET_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$bamboo_AWS_REGION
+
+
 docker build . -t browseui &&
 CID=$(docker create browseui) &&
 docker cp "${CID}":app/build ./dist &&

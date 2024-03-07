5ba2e354a06b8846175dd551c628abd4d447ccec
alansubedi123 <alansubedi123@gmail.com>
2024-03-07 10:48:59 -0600

    fixed pdf url bug, a image icon pops up still, need to fix it

--- src/components/appComponents/ResultsTable.js
+++ src/components/appComponents/ResultsTable.js
@@ -159,6 +159,18 @@ const ResultsTable = ({ skip, setSkipTrue, setSkipFalse }) => {
     };
   }, [progress]);
 
+
+  // useEffect(()=>{
+  //   const url = window.location.href
+  //   if (url.indexOf(".") !== -1){
+  //     const id = url.split("#")[1]
+  //     setFilePath(`${config.cloudWatchUrlBase}${id}`)
+  //     setImg(id);
+  //     setRowData(response);
+  //     setOpen(true);
+  //   }
+  // })
+
   const updateBrowserURL = (id) => {
     // Modify the URL
     var newUrl = "browseui/#" + id;
@@ -235,8 +247,20 @@ const ResultsTable = ({ skip, setSkipTrue, setSkipFalse }) => {
     setScale(1);
   };
 
+  useEffect(()=>{
+    const url = window.location.href
+    if (url.indexOf(".") !== -1){
+      const id = url.split("#")[1]
+      setFilePath(`${config.cloudWatchUrlBase}${id}`)
+      setImg(id);
+      setRowData(response[0]);
+      setOpen(true);
+    }
+  })
+
   const handleClose = (rowData) => {
     let img = rowData.Key;
+    console.log("inside handleClose",img)
     //To set the next image to original size
     setScale(1);
     const desiredPath = img.substring(0, img.lastIndexOf("/") + 1);
